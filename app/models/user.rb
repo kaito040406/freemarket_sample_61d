e00e8c332c75b8a676674414cb41783f4fd87088
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-29 21:24:52 +0900

    サインインのパス、入力要素追加
    サインインのパスと入力要素を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,  :validatable
+
+  validates :nick_name, presence: true
+  validates :sur_name, presence: true
+  validates :fr_name, presence: true
+  validates :sur_name_yomi, presence: true
+  validates :first_name_yomi, presence: true
+  validates :birthday_year, presence: true
+  validates :birthday_manth, presence: true
+  validates :birthday_day, presence: true 
 end
