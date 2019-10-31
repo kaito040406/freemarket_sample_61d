d7db66084c526bf1ce90ab1da1e5045858ee3143
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-10-31 15:01:55 +0900

    バックアップファイルのファイル名変更、表示用のコントローラーの記述の削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,10 @@ class ProductsController < ApplicationController
   end
   
   def new
-
+    @product = Product.new
   end
 
   def new_copy
     @product = Product.new
-    @prefecture = Prefecture.data[1]
   end
 end
