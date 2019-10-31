e1c9e36237b88933ca14871253e6bc725fcb5784
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-10-31 13:08:57 +0900

    active_hashのPrefecture作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
   def index
+    
   end
   
   def new
+
+  end
+
+  def new_copy
+    @product = Product.new
+    @prefecture = Prefecture.data[1]
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  #DBのカラム名"categry", # %option{value: ""} \---    <-プレースホルダ
+  enum categry:  {"レディース": 1,
+                   "メンズ": 2, 
+                   "ベビー・キッズ": 3,
+                   "インテリア・住まい・小物": 4,
+                    "本・音楽・ゲーム": 5,
+                   "おもちゃ・ホビー・グッズ": 1328,
+                   "コスメ・香水・美容": 6,
+                   "家電・スマホ・カメラ": 7,
+                   "スポーツ・レジャー": 8,
+                   "ハンドメイド": 9,
+                   "チケット": 1027,
+                   "自動車・オートバイ": 1318,
+                   "その他": 10}
 end
