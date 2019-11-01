5a7306c3ef43489ac2b2317388951c93b1a30877
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-01 19:03:00 +0900

    画像保存成功
    いったんform_for @productのf.file_field の multiple: true を削除し、単独の画像扱いとしている

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,35 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-  def new_copy
-    @product = Product.new
+  def create
+    # @product = Product.new(product_params)
+    # if @product.save
+      @product_image = ProductImage.new(product_image_params)
+      if @product_image.save
+      else
+        # @product.delete
+      end
+    # else
+
+    # end
+    render :new
+  end
+
+  private
+
+  # def product_params
+  #   params[:product][:seller_id] = 1
+  #   params[:product][:size] = 1
+  #   params[:product][:delivery_method] = 1
+  #   params[:product][:date] = Date.current
+  #   params.require(:product).permit(:seller_id, :name, :text, :categry, :status, :size, :date, :delivery_fee, :delivery_method, :delivery_from, :extimated_delivery_date, :price)
+  # end
+
+  def product_image_params
+    params[:count]= 1
+    column_product_image = params[:product][:product_image]
+    kaerichi = params.permit(:count).merge(product_id: 2, product_image: column_product_image) #productが保存されそのidができた後、それを呼び出さなければならない
+    binding.pry
+    return kaerichi
   end
 end
