3ae51534f32a9a2be94504ed1c2a140671567546
19990824 <wataru824@icloud.com>
2019-11-01 16:02:26 +0900

    パンくず機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :users 
+  resources :users 
   resources :products
 end
