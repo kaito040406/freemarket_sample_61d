a826adbe5b806f82a500256d982b98e9f7b83fa0
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-02 17:44:06 +0900

    記述の整頓・メモ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,17 +24,20 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params[:product][:seller_id] = 1
+    # ダミーデータ挿入
+    params[:product][:seller_id] = 1#current_userが入るように！<-次やるのはここ予定
     params[:product][:size] = 1
     params[:product][:delivery_method] = 1
     params[:product][:date] = Date.current
+    # ダミーデータ挿入終わり
     params.require(:product).permit(:seller_id, :name, :text, :categry, :status, :size, :date, :delivery_fee, :delivery_method, :delivery_from, :extimated_delivery_date, :price)
   end
 
   def product_image_params
+    # ダミーデータ挿入
     params[:count]= 1
+    # ダミーデータ挿入終わり
     column_product_image = params[:product][:product_image]
-    kaerichi = params.permit(:count).merge(product_id: 2, product_image: column_product_image) #productが保存されそのidができた後、それを呼び出さなければならない
-    return kaerichi
+    params.permit(:count).merge(product_id: @product.id, product_image: column_product_image) #productが保存されそのidができた後、それを呼び出さなければならない
   end
 end
