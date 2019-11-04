a5b1d6eacb250359e3998d60272245265771833b
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-04 18:10:05 +0900

    セレクトボックスの選択値及び選択値無しの検出

--- app/assets/javascripts/product_create.js
+++ app/assets/javascripts/product_create.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function(){
+  //手数料と利益の計算
   $('#product_price').on('keyup', function(e){
     let product_fee_rate = 0.1
     let product_price = $('#product_price').val();
@@ -6,5 +7,13 @@ $(document).on('turbolinks:load', function(){
     let product_gain = product_price - product_fee;
     $('#product-fee').html(product_fee);
     $('#product-gain').html(product_gain);
+  });  
+
+  $('#product_categry').change(function() {
+    let selection = $('option:selected').val();
+    console.log(selection);
+    if (!selection) {
+      console.log('default');
+    }
   });
 });
