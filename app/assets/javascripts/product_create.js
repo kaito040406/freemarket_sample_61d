3afdb94f4ca028f6ee3ac379fb3cfc007ce3c147
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-04 16:35:36 +0900

    CSSセレクタの整頓・修正

--- app/assets/javascripts/product_create.js
+++ app/assets/javascripts/product_create.js
@@ -4,7 +4,7 @@ $(document).on('turbolinks:load', function(){
     let product_price = $('#product_price').val();
     let product_fee = Math.floor(product_price * product_fee_rate);
     let product_gain = product_price - product_fee;
-    $('.product-fee').html(product_fee);
-    $('.product-gain').html(product_gain);
+    $('#product-fee').html(product_fee);
+    $('#product-gain').html(product_gain);
   });
 });
