749f2a90be395e40cb9ff6c3ad763b00d81db08c
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-04 15:34:40 +0900

    手数料計算、小数点以下は切り捨て

--- app/assets/javascripts/product_create.js
+++ app/assets/javascripts/product_create.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   $('#product_price').on('keyup', function(e){
     let product_fee_rate = 0.1
     let product_price = $('#product_price').val();
-    let product_fee = product_price * product_fee_rate;
+    let product_fee = Math.floor(product_price * product_fee_rate);
     let product_gain = product_price - product_fee;
     $('.product-fee').html(product_fee);
     $('.product-gain').html(product_gain);
