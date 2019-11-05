7164d7d144093e5b4d6ff069cdc84f88fec2a05c
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-05 21:16:09 +0900

    インデックス修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   def index
     @products = Product.limit(10).order('created_at DESC')
     @images = ProductImage.limit(10).order("created_at DESC")
