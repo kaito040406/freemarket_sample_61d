f77b420b54cfaa8f0e7de8b137bb63826b34f0c1
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-08 00:45:28 +0900

    ストレージ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  #storage :file
+  storage :file
 
-  storage :fog
+  #storage :fog
 
 
   # Override the directory where uploaded files will be stored.
