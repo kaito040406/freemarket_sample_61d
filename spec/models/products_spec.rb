88fb2837e2138d68bea2bed8c2e54a430ab15197
kaito040406 <55653238+kaito040406@users.noreply.github.com>
2019-11-08 12:41:35 +0900

    Revert "商品詳細ページの単体テスト"

--- spec/models/products_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-RSpec.describe Product, type: :model do
-  describe '#show' do 
-    if "is invalid without a seller_id"
-      seller_id = build(:Product, seller_id: 1)
-      expect(seller_id).to be_valid
-    end
-  end
-end        
