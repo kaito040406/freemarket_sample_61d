9dbf8bf43a87ab826a879f35b23d3c6a360bc535
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-09 00:17:28 +0900

    一部修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,8 +40,11 @@ class ProductsController < ApplicationController
   
   def show
     @product=Product.find(params[:id])
+
     @image = ProductImage.find_by(product_id: params[:id])
+
     @user = User.find_by(id: @product.seller_id)
+    
   end  
 
   private
