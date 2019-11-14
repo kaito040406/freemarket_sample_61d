11cba1fda88e39b860371a0f88b3802361965d1a
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-11-14 16:19:11 +0900

    商品編集02

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ class ProductsController < ApplicationController
   end
 
   def create
-    binding.pry
     @product = Product.new(product_params)
     #@product.user = current_user
     
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :products, only: [:new, :create, :edit, :destroy]
-
     namespace :api do
       resources :products, defaults: { format: 'json' } do
         collection do
