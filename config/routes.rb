b6d2c10b1c5754e18488bf1b7dcda0842ed3e0e7
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-11-15 14:16:50 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get 'my_details'
       get 'purchase_confirmation'
       get 'mypage'
-      patch 'updete'
+      get 'update'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
