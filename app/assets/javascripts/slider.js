a6ecd6e5659deb0afad476dca3c0298dc5a33bc2
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-16 13:22:59 +0900

    修正

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,11 +1,10 @@
-
-$(function(){
+$(document).on('turbolinks:load', function(){
   $('.slick').slick({
     autoplay:true,  
     dots:true,    
   });
 });
-$(function(){
+$(document).on('turbolinks:load', function(){
   $("#slider").slick({
     prevArrow: '<img src=""  class="slide-arrow prev-arrow">',
     nextArrow: '<img src="" class="slide-arrow next-arrow">'
