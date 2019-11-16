c7a30063810e56513bceeec707e8a69f1f3d1677
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-11-16 12:44:11 +0900

    編集フォームでは最初から手数料と利益を表示

--- app/assets/javascripts/product_edit.js
+++ app/assets/javascripts/product_edit.js
@@ -1,3 +1,21 @@
+//値段に合わせ手数料と利益を更新する関数
+function calcFeeGain(){
+  let product_fee_rate = 0.1
+  let product_price = $('#product_price').val();
+  let product_fee = Math.floor(product_price * product_fee_rate);
+  let product_gain = product_price - product_fee;
+  $('#product-fee').html(product_fee);
+  $('#product-gain').html(product_gain);
+}
+/////カテゴリ生成
+function appendCategory(ct){
+  var html = `
+              <option value="${ct.name}" id = "${ct.id}">${ct.name}</option>
+              `
+  return html;
+}
+
+//////////////ここから本体
 $(document).on('turbolinks:load', function(){
   //newフォームからは実行されない
   let pathSelf =location.pathname;
@@ -6,135 +24,13 @@ $(document).on('turbolinks:load', function(){
     return false;
   }
   
-  //1枚以上の画像はあるため最初はプレースホルダ非表示
+  //画像があるためイメージボックスのプレースホルダ非表示
   $('.img-uploader-dropbox pre.edit-form').hide();
-
-  function appendCategory(ct){
-    var html = `
-                <option value="${ct.name}" id = "${ct.id}">${ct.name}</option>
-                `
-    return html;
-    }
-
-  $('#category_parent').change(function() {
-      var parent_name = $(this).val();
-      user_id = $(".select-wrap").attr("id");
-      if(parent_name == "レディース"){
-        parent_id = 1;
-      }
-      if(parent_name == "メンズ"){
-        parent_id = 200;
-      }
-      if(parent_name == "ベビー・キッズ"){
-        parent_id = 345;
-      }
-      if(parent_name == "インテリア・住まい・小物"){
-        parent_id = 480;
-      }
-      if(parent_name == "本・音楽・ゲーム"){
-        parent_id = 623;
-      }
-      if(parent_name == "おもちゃ・ホビー・グッズ"){
-        parent_id = 683;
-      }
-      if(parent_name == "コスメ・香水・美容"){
-        parent_id = 796;
-      }
-      if(parent_name == "家電・スマホ・カメラ"){
-        parent_id = 896;
-      }
-      if(parent_name == "スポーツ・レジャー"){
-        parent_id = 982;
-      }
-      if(parent_name == "チケット"){
-        parent_id = 1143;
-      }
-      if(parent_name == "自動車・オートバイ"){
-        parent_id = 1202;
-      }
-      if(parent_name == "その他"){
-        parent_id = 1264;
-      }
-      $.ajax({
-          type: 'GET',
-          url: "/users/" + user_id + "/api/products/child",
-          dataType: 'json',
-          data: {id: parent_id}
-      })
-      .done(function(categories){
-        if($('#ct_no_2').val() != null){
-          $('#ct_no_2').remove();
-        }
-        if($('#ct_no_3').val() != null){
-          $('#ct_no_3').remove();
-        }
-        html_head = `
-                    <div class="form-input-t_2" id="ct_no_2" value="ct_no_2">
-                      <div class="select-wrap" id="1">
-                      <i class="fa fa-chevron-down"></i>
-                    <select class="category_child" id="category_child" name="child">
-                    <option value="---" id = "---">---</option>
-                    `
-        ap_html = html_head
-        categories.forEach(function(category){
-          ct_html = appendCategory(category)
-          ap_html = ap_html + ct_html
-        })
-        html_foot=`
-                    </select>
-                    </div>
-                    </div>
-                  `
-        html = ap_html + html_foot;
-        $('.ct_box_k').append(html);
-      })
-      .fail((data) => {
-        //失敗した場合の処理
-        console.log(data.responseText);  //レスポンス文字列を表示
-      })
-  });
   
-  $(this).on("change", "#category_child", function() {
-    var child_name = $(this).val();
-    if(child_name != "---"){
-      $.ajax({
-          type: 'GET',
-          url: "/users/" + user_id + "/api/products/grand_child",
-          data: {id: child_name},
-          dataType: 'json'
-      })
-      .done(function(categories) {
-        if($('#ct_no_3').val() != null){
-          $('#ct_no_3').remove();
-        }
-        html_head_g = `
-                    <div class="form-input-t_3" id="ct_no_3" value="ct_no_3">
-                      <div class="select-wrap" id="1">
-                      <i class="fa fa-chevron-down"></i>
-                    <select class="category_grand_child" id="category_grand_child" name="grand" >
-                    <option value="---" id = "---">---</option>
-                    `
-        ap_html_g = html_head_g
-        categories.forEach(function(category){
-          ct_html_g = appendCategory(category)
-          ap_html_g = ap_html_g + ct_html_g
-        })
-        html_foot_g=`
-                    </select>
-                    </div>
-                    </div>
-                  `
-        html_g = ap_html_g + html_foot_g;
-        $('.ct_box_k').append(html_g);
-      })
-      .fail(function() {
-      });
-    }
-    if(child_name == "---"){
-      $('#ct_no_3').remove();
-    }
-  });
+  //手数料と利益を表示
+  calcFeeGain();
 
+  //出品内容の親子孫のカテゴリセレクタを生成
 
 let DeliveryMethodSelectBoxHTML = `
 <div class="form-input-t">
@@ -359,7 +255,7 @@ function readLabelIndex(){
     return labelIndex;
   }
 }
-//hidden属性で送られるcountの値を今あるimgの連番で振り直し（途中のイメージを削除された時のため）
+///////ここまでカテゴリ関連/////////
 
 
 ////////ここからイメージボックス関連
@@ -415,7 +311,8 @@ function readLabelIndex(){
 
   });
 
-  //削除ボタンを押した時の処理
+  //////要素ごとのイベント応答群
+  //画像サムネイルの削除ボタンを押した時の処理
   $(document).off('click');//イベント多重化防止
   $(document).on('click', '.img-delete-btn', function(e) {//なぜ$()->$(document)だといけたのか未理解
     e.preventDefault();
@@ -431,22 +328,135 @@ function readLabelIndex(){
     overwriteLabel(labelIndex);
     overwriteHiddenCountAll();
   });
-
+  ///カテゴリセレクトボックス:親->子->孫と出現する
+  $('#category_parent').change(function() {
+    var parent_name = $(this).val();
+    user_id = $(".select-wrap").attr("id");
+    if(parent_name == "レディース"){
+      parent_id = 1;
+    }
+    if(parent_name == "メンズ"){
+      parent_id = 200;
+    }
+    if(parent_name == "ベビー・キッズ"){
+      parent_id = 345;
+    }
+    if(parent_name == "インテリア・住まい・小物"){
+      parent_id = 480;
+    }
+    if(parent_name == "本・音楽・ゲーム"){
+      parent_id = 623;
+    }
+    if(parent_name == "おもちゃ・ホビー・グッズ"){
+      parent_id = 683;
+    }
+    if(parent_name == "コスメ・香水・美容"){
+      parent_id = 796;
+    }
+    if(parent_name == "家電・スマホ・カメラ"){
+      parent_id = 896;
+    }
+    if(parent_name == "スポーツ・レジャー"){
+      parent_id = 982;
+    }
+    if(parent_name == "チケット"){
+      parent_id = 1143;
+    }
+    if(parent_name == "自動車・オートバイ"){
+      parent_id = 1202;
+    }
+    if(parent_name == "その他"){
+      parent_id = 1264;
+    }
+    $.ajax({
+        type: 'GET',
+        url: "/users/" + user_id + "/api/products/child",
+        dataType: 'json',
+        data: {id: parent_id}
+    })
+    .done(function(categories){
+      if($('#ct_no_2').val() != null){
+        $('#ct_no_2').remove();
+      }
+      if($('#ct_no_3').val() != null){
+        $('#ct_no_3').remove();
+      }
+      html_head = `
+                  <div class="form-input-t_2" id="ct_no_2" value="ct_no_2">
+                    <div class="select-wrap" id="1">
+                    <i class="fa fa-chevron-down"></i>
+                  <select class="category_child" id="category_child" name="child">
+                  <option value="---" id = "---">---</option>
+                  `
+      ap_html = html_head
+      categories.forEach(function(category){
+        ct_html = appendCategory(category)
+        ap_html = ap_html + ct_html
+      })
+      html_foot=`
+                  </select>
+                  </div>
+                  </div>
+                `
+      html = ap_html + html_foot;
+      $('.ct_box_k').append(html);
+    })
+    .fail((data) => {
+      //失敗した場合の処理
+      console.log(data.responseText);  //レスポンス文字列を表示
+    })
+  });
+  $(this).on("change", "#category_child", function() {
+    var child_name = $(this).val();
+    if(child_name != "---"){
+      $.ajax({
+          type: 'GET',
+          url: "/users/" + user_id + "/api/products/grand_child",
+          data: {id: child_name},
+          dataType: 'json'
+      })
+      .done(function(categories) {
+        if($('#ct_no_3').val() != null){
+          $('#ct_no_3').remove();
+        }
+        html_head_g = `
+                    <div class="form-input-t_3" id="ct_no_3" value="ct_no_3">
+                      <div class="select-wrap" id="1">
+                      <i class="fa fa-chevron-down"></i>
+                    <select class="category_grand_child" id="category_grand_child" name="grand" >
+                    <option value="---" id = "---">---</option>
+                    `
+        ap_html_g = html_head_g
+        categories.forEach(function(category){
+          ct_html_g = appendCategory(category)
+          ap_html_g = ap_html_g + ct_html_g
+        })
+        html_foot_g=`
+                    </select>
+                    </div>
+                    </div>
+                  `
+        html_g = ap_html_g + html_foot_g;
+        $('.ct_box_k').append(html_g);
+      })
+      .fail(function() {
+      });
+    }
+    if(child_name == "---"){
+      $('#ct_no_3').remove();
+    }
+  });
+  //送料負担が選択されたら発送方法セレクトボックス出現
   $('#product_delivery_fee').change(function() {
     let selection = $('option:selected').val();
     $('#product_delivery_fee').after(DeliveryMethodSelectBoxHTML);
-    if (!selection) {
+    if (!selection) {//<-??忘れてしまった
       //
     }
   });
-
-  //手数料と利益の計算
+  //出品価格が変化したら手数料と利益を更新
   $('#product_price').on('keyup', function(e){
-    let product_fee_rate = 0.1
-    let product_price = $('#product_price').val();
-    let product_fee = Math.floor(product_price * product_fee_rate);
-    let product_gain = product_price - product_fee;
-    $('#product-fee').html(product_fee);
-    $('#product-gain').html(product_gain);
+    calcFeeGain();
   });
+
 });
