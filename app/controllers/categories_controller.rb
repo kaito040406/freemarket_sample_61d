17d3cd46397b144651fa72d3b3a4804251e9ef40
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-16 15:31:21 +0900

    edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,8 +4,9 @@ class CategoriesController < ApplicationController
   def index
     @category_parent = Category.where(ancestry: nil)
   end
-
+  
   def show
+    category_check(@category)
   end
 
   private
@@ -14,5 +15,31 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
-  
+  def category_check(cat)
+    anc = cat.ancestry
+    match_id = []
+    if anc.blank?
+      anc_set = "^#{cat.id}/"
+      reg = Regexp.new(anc_set)
+      not_null_categories = Category.where.not("ancestry IS NULL")
+      not_null_categories.each do |c|
+        if c[:ancestry].match(reg)
+          match_id << c.id
+        end
+      end
+      @products = Product.where(category_id: match_id).order("id DESC")
+    elsif anc.match(/\//)
+      @products = Product.where(category_id: cat.id).order("id DESC")
+    else
+      parent = anc
+      child = cat.id
+      anc_set = "#{parent}/#{child}"
+      Category.all.each do |c|
+        if c[:ancestry] == anc_set
+          match_id << c.id
+        end
+      end
+      @products = Product.where(category_id: match_id).order("id DESC")
+    end
+  end
 end
