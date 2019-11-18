50a4d779d5b92f0d9588e03d404c2f44c5d81bdc
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-18 16:54:16 +0900

    card_test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#new' do
+  # 1  以下の値にはバリデーションがかかっている
+    it "is valid with a user_id, customer_id, card_id" do
+      card = build(:card)
+      expect(card).to be_valid
+    end
+  end
 end
