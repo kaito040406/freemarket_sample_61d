079f4919c44ad55f0744ecdd1444b390951382b8
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-18 19:24:09 +0900

    cards_test_2

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 describe Cards do
   describe '#create' do
   # 1  以下の値にはバリデーションがかかっている
-    it "is valid with a user_id, customer_id, card_id" do
+    it "is valid with a customer_id, card_id" do
       card = build(:cards)
       expect(card).to be_valid
     end
+
+  # 2 customer_idが空の場合登録できない
+    it "is invalid without a customer_id" do
+      card = build(:cards, customer_id: nil)
+      card.valid?
+      binding.pry
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+    
   end
 end
