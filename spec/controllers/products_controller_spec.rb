8640a6b865473d6f5197f976cfcd964b2b63c3cd
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-19 18:25:13 +0900

    inddx_test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -7,10 +7,10 @@ describe ProductsController do
     expect(response).to render_template :new
     end
   end
-    describe 'GET #index' do
-      it "renders the :index template" do
-        get :index
-        expect(response).to render_template :index
+  describe 'GET #index' do
+  it "renders the :index template" do
+    get :index
+    expect(response).to render_template :index
     end
   end
 end 
