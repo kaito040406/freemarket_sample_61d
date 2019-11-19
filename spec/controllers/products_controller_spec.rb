d7d6ed8ccfa1f305b103dfe912ac315f715d7826
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-20 00:01:56 +0900

    products_test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -11,13 +11,16 @@ describe ProductsController do
   it "renders the :index template" do
     get :index
     expect(response).to render_template :index
-    
+  end
+end
+
     describe 'get #show' do
       it "renders the :show template" do
         product = create(:product)
         get :show, params: {  id: product }
         expect(response).to render_template :show
       end
+    end
 
     describe 'delete #destroy' do
       it "renders to root_path" do
@@ -34,8 +37,6 @@ describe ProductsController do
         expect(response).to render_template :update
       end
     end
-    end
-  end
   # describe 'POST #create' do
   #   let(:params) { {product: attributes_for(:product) } }
   #   it "renders the :create template" do
@@ -43,6 +44,4 @@ describe ProductsController do
   #       expect(response).to render_template :create
   #   end
   # end
-end 
-  
-end    
+end  
