ab895136171c0f43ff04238d3f14f4183acf5990
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-19 17:32:01 +0900

    products_test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,25 +1,3 @@
 require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
-    describe 'get #show' do
-      it "renders the :show template" do
-        product = create(:product)
-        get :show, params: {  id: product }
-        expect(response).to render_template :show
-      end
-
-    describe 'delete #destroy' do
-      it "renders to root_path" do
-        product = create(:product)
-        delete :destroy, params: {id: product.id }
-        expect(response).to redirect_to root_path
-      end    
-    end
-
-    describe 'get #update' do 
-      it "renders the :update template" do
-        product = create(:product)
-        get :update, params: {id: product}
-        expect(response).to render_template :update
-      end
-    end
-end    
+end 
