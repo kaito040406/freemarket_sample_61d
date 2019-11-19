5dae8f533a030429acd4d47317044deaa507b0d6
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-19 23:59:35 +0900

    products_test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,18 +1,41 @@
 require 'rails_helper'
 
 describe ProductsController do
-  # describe 'GET #new' do
-  # it "renders the :new template" do
-  #   get :new
-  #   expect(response).to render_template :new
-  #   end
-  # end
-  # describe 'GET #index' do
-  # it "renders the :index template" do
-  #   get :index
-  #   expect(response).to render_template :index
-  #   end
-  # end
+  describe 'GET #new' do
+  it "renders the :new template" do
+    get :new
+    expect(response).to render_template :new
+    end
+  end
+  describe 'GET #index' do
+  it "renders the :index template" do
+    get :index
+    expect(response).to render_template :index
+    
+    describe 'get #show' do
+      it "renders the :show template" do
+        product = create(:product)
+        get :show, params: {  id: product }
+        expect(response).to render_template :show
+      end
+
+    describe 'delete #destroy' do
+      it "renders to root_path" do
+        product = create(:product)
+        delete :destroy, params: {id: product.id }
+        expect(response).to redirect_to root_path
+      end    
+    end
+
+    describe 'get #update' do 
+      it "renders the :update template" do
+        product = create(:product)
+        get :update, params: {id: product}
+        expect(response).to render_template :update
+      end
+    end
+    end
+  end
   # describe 'POST #create' do
   #   let(:params) { {product: attributes_for(:product) } }
   #   it "renders the :create template" do
@@ -21,3 +44,5 @@ describe ProductsController do
   #   end
   # end
 end 
+  
+end    
