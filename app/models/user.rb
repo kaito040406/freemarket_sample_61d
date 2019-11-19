2daf38d9f37b79e11a385974ea0bc9838c508b5e
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-19 15:55:26 +0900

    users_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,24 @@ class User < ApplicationRecord
          
 
 
-  validates :nick_name, presence: true
-  validates :sur_name, presence: true
-  validates :fr_name, presence: true
-  validates :sur_name_yomi, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_yomi, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday_year, presence: true, numericality: true
-  validates :birthday_manth, presence: true, numericality: true
-  validates :birthday_day, presence: true, numericality: true
-  validates :tel_number, presence: true, length: { is: 11 }, numericality: true
-  validates :certification, presence: true, length: { is: 4 }, numericality: true
-  validates :address_number, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/}
-  validates :address_ken, presence: true
-  validates :address_city, presence: true
-  validates :address_banch, presence: true
+  validates :nick_name,         presence: true
+  validates :email,             presence: true
+  validates :sur_name,          presence: true
+  validates :fr_name,           presence: true
+  validates :sur_name_yomi,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_yomi,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday_year,     presence: true, numericality: true
+  validates :birthday_manth,    presence: true, numericality: true
+  validates :birthday_day,      presence: true, numericality: true
+  validates :tel_number,        presence: true, length: { is: 11 }, numericality: true
+  validates :certification,     presence: true, length: { is: 4 }, numericality: true
+  validates :address_number,    presence: true, format: {with: /\A\d{3}[-]\d{4}\z/}
+  validates :address_ken,       presence: true
+  validates :address_city,      presence: true
+  validates :address_banch,     presence: true
 
+
+  
   has_many :products
   has_many :produst_images
   has_many :cards
