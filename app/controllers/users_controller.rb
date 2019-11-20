4fc5c86b5635f8a52b6df0550e2f34d0dd39f293
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-11-20 16:44:06 +0900

    部分テンプレートのrender

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,24 @@ class UsersController < ApplicationController
 
   def show
     @count = Product.where(seller_id: current_user.id).length
+    @search = Product.ransack(params[:id])
   end
 
   def edit
   end
 
   def log_out
+    @search = Product.ransack(params[:id])
   end
 
   def profile
+    @search = Product.ransack(params[:id])
   end
   
   def identification
     @data = Prefecture.all
     @user = current_user
+    @search = Product.ransack(params[:id])
   end
 
   def failkure
@@ -28,6 +32,7 @@ class UsersController < ApplicationController
   def progress
     @products = Product.where(seller_id: current_user.id)
     @image = ProductImage.where(product_id: @products.ids)
+    @search = Product.ransack(params[:id])
   end
 
   def privacy
