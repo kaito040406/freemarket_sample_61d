05efeb55748c9a72df96dab74fb380567edbcf4f
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-20 16:10:59 +0900

    修正

--- app/assets/javascripts/product_edit.js
+++ app/assets/javascripts/product_edit.js
@@ -154,7 +154,6 @@ function readLabelIndex(){
                     `
         ap_html_g = html_head_g
         categories.forEach(function(category){
-          console.log(category)
           ct_html_g = appendCategory(category)
           ap_html_g = ap_html_g + ct_html_g
         })
@@ -177,7 +176,6 @@ function readLabelIndex(){
 
 //////////////ここから本体
 $(document).on('turbolinks:load', function(){
-  console.log("ok");
   //newフォームからは実行されない
   let pathSelf =location.pathname;
   if (pathSelf.match(/new/) != null) {
@@ -272,125 +270,125 @@ $(document).on('turbolinks:load', function(){
   //   overwriteHiddenCountAll();
   // });
   ///カテゴリセレクトボックス:親->子->孫と出現する
-  $('#categry_parent').change(function() {
-
-    var parent_name = $(this).val();
-    user_id = $(".select-wrap").attr("id");
-    if(parent_name == "レディース"){
-      parent_id = 1;
-    }
-    if(parent_name == "メンズ"){
-      parent_id = 200;
-    }
-    if(parent_name == "ベビー・キッズ"){
-      parent_id = 345;
-    }
-    if(parent_name == "インテリア・住まい・小物"){
-      parent_id = 480;
-    }
-    if(parent_name == "本・音楽・ゲーム"){
-      parent_id = 623;
-    }
-    if(parent_name == "おもちゃ・ホビー・グッズ"){
-      parent_id = 683;
-    }
-    if(parent_name == "コスメ・香水・美容"){
-      parent_id = 796;
-    }
-    if(parent_name == "家電・スマホ・カメラ"){
-      parent_id = 896;
-    }
-    if(parent_name == "スポーツ・レジャー"){
-      parent_id = 982;
-    }
-    if(parent_name == "チケット"){
-      parent_id = 1143;
-    }
-    if(parent_name == "自動車・オートバイ"){
-      parent_id = 1202;
-    }
-    if(parent_name == "その他"){
-      parent_id = 1264;
-    }
-    $.ajax({
-        type: 'GET',
-        url: "/users/" + user_id + "/api/products/child",
-        dataType: 'json',
-        data: {id: parent_id}
-    })
-    .done(function(categories){
-      console.log("ok11")
-      if($('#ct_no_2').val() != null){
-        $('#ct_no_2').remove();
-      }
-      if($('#ct_no_3').val() != null){
-        $('#ct_no_3').remove();
-      }
-      html_head = `
-                  <div class="form-input-t_2" id="ct_no_2" value="ct_no_2">
-                    <div class="select-wrap" id="1">
-                    <i class="fa fa-chevron-down"></i>
-                  <select class="category_child" id="category_child" name="child">
-                  <option value="---" id = "---">---</option>
-                  `
-      ap_html = html_head
-      categories.forEach(function(category){
-        ct_html = appendCategory(category)
-        ap_html = ap_html + ct_html
-      })
-      html_foot=`
-                  </select>
-                  </div>
-                  </div>
-                `
-      html = ap_html + html_foot;
-      $('.ct_box_k').append(html);
-    })
-    .fail((data) => {
-      //失敗した場合の処理
-      // console.log(data.responseText);  //レスポンス文字列を表示
-    })
-  });
-  $(this).on("change", "#category_child", function() {
-    var child_name = $(this).val();
-    if(child_name != "---"){
-      $.ajax({
-          type: 'GET',
-          url: "/users/" + user_id + "/api/products/grand_child",
-          data: {id: child_name},
-          dataType: 'json'
-      })
-      .done(function(categories) {
-        if($('#ct_no_3').val() != null){
-          $('#ct_no_3').remove();
-        }
-        html_head_g = `
-                    <div class="form-input-t_3" id="ct_no_3" value="ct_no_3">
-                      <div class="select-wrap" id="1">
-                      <i class="fa fa-chevron-down"></i>
-                    <select class="category_grand_child" id="category_grand_child" name="grand" >
-                    <option value="---" id = "---">---</option>
-                    `
-        ap_html_g = html_head_g
-        categories.forEach(function(category){
-          ct_html_g = appendCategory_g(category)
-          ap_html_g = ap_html_g + ct_html_g
-        })
-        html_foot_g=`
-                    </select>
-                    </div>
-                    </div>
-                  `
-        html_g = ap_html_g + html_foot_g;
-        $('.ct_box_k').append(html_g);
-      })
-      .fail(function() {
-      });
-    }
-    if(child_name == "---"){
-      $('#ct_no_3').remove();
-    }
-  });
+  // $('#categry_parent').change(function() {
+  //   console.log("ok11")
+  //   var parent_name = $(this).val();
+  //   user_id = $(".select-wrap").attr("id");
+  //   if(parent_name == "レディース"){
+  //     parent_id = 1;
+  //   }
+  //   if(parent_name == "メンズ"){
+  //     parent_id = 200;
+  //   }
+  //   if(parent_name == "ベビー・キッズ"){
+  //     parent_id = 345;
+  //   }
+  //   if(parent_name == "インテリア・住まい・小物"){
+  //     parent_id = 480;
+  //   }
+  //   if(parent_name == "本・音楽・ゲーム"){
+  //     parent_id = 623;
+  //   }
+  //   if(parent_name == "おもちゃ・ホビー・グッズ"){
+  //     parent_id = 683;
+  //   }
+  //   if(parent_name == "コスメ・香水・美容"){
+  //     parent_id = 796;
+  //   }
+  //   if(parent_name == "家電・スマホ・カメラ"){
+  //     parent_id = 896;
+  //   }
+  //   if(parent_name == "スポーツ・レジャー"){
+  //     parent_id = 982;
+  //   }
+  //   if(parent_name == "チケット"){
+  //     parent_id = 1143;
+  //   }
+  //   if(parent_name == "自動車・オートバイ"){
+  //     parent_id = 1202;
+  //   }
+  //   if(parent_name == "その他"){
+  //     parent_id = 1264;
+  //   }
+  //   $.ajax({
+  //       type: 'GET',
+  //       url: "/users/" + user_id + "/api/products/child",
+  //       dataType: 'json',
+  //       data: {id: parent_id}
+  //   })
+  //   .done(function(categories){
+      
+  //     if($('#ct_no_2').val() != null){
+  //       $('#ct_no_2').remove();
+  //     }
+  //     if($('#ct_no_3').val() != null){
+  //       $('#ct_no_3').remove();
+  //     }
+  //     html_head = `
+  //                 <div class="form-input-t_2" id="ct_no_2" value="ct_no_2">
+  //                   <div class="select-wrap" id="1">
+  //                   <i class="fa fa-chevron-down"></i>
+  //                 <select class="category_child" id="category_child" name="child">
+  //                 <option value="---" id = "---">---</option>
+  //                 `
+  //     ap_html = html_head
+  //     categories.forEach(function(category){
+  //       ct_html = appendCategory(category)
+  //       ap_html = ap_html + ct_html
+  //     })
+  //     html_foot=`
+  //                 </select>
+  //                 </div>
+  //                 </div>
+  //               `
+  //     html = ap_html + html_foot;
+  //     $('.ct_box_k').append(html);
+  //   })
+  //   .fail((data) => {
+  //     //失敗した場合の処理
+  //     // console.log(data.responseText);  //レスポンス文字列を表示
+  //   })
+  // });
+  // $(this).on("change", "#category_child", function() {
+  //   var child_name = $(this).val();
+  //   if(child_name != "---"){
+  //     $.ajax({
+  //         type: 'GET',
+  //         url: "/users/" + user_id + "/api/products/grand_child",
+  //         data: {id: child_name},
+  //         dataType: 'json'
+  //     })
+  //     .done(function(categories) {
+  //       if($('#ct_no_3').val() != null){
+  //         $('#ct_no_3').remove();
+  //       }
+  //       html_head_g = `
+  //                   <div class="form-input-t_3" id="ct_no_3" value="ct_no_3">
+  //                     <div class="select-wrap" id="1">
+  //                     <i class="fa fa-chevron-down"></i>
+  //                   <select class="category_grand_child" id="category_grand_child" name="grand" >
+  //                   <option value="---" id = "---">---</option>
+  //                   `
+  //       ap_html_g = html_head_g
+  //       categories.forEach(function(category){
+  //         ct_html_g = appendCategory_g(category)
+  //         ap_html_g = ap_html_g + ct_html_g
+  //       })
+  //       html_foot_g=`
+  //                   </select>
+  //                   </div>
+  //                   </div>
+  //                 `
+  //       html_g = ap_html_g + html_foot_g;
+  //       $('.ct_box_k').append(html_g);
+  //     })
+  //     .fail(function() {
+  //     });
+  //   }
+  //   if(child_name == "---"){
+  //     $('#ct_no_3').remove();
+  //   }
+  // });
 
   //送料負担が選択されたら発送方法セレクトボックス出現
   //!修正要!無限に増えてしまっている
