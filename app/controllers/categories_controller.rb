4c4a89fea0d2148641210b649dc15ce524de54a3
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-21 15:32:31 +0900

    edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,14 +7,27 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    @products = Product.where(id: ">200")
     @images = ProductImage.where(params[:id])
+    @product = Product
   end
 
   private
 
   def set_category
     @category = Category.find(params[:id])
+    women = Product.where(parent: "レディース")
+    men = Product.where(parent: "メンズ")
+    kids = Product.where(parent: "ベビー・キッズ")
+    items = Product.where(parent: "インテリア・住まい・小物")
+    books = Product.where(parent: "本・音楽・ゲーム")
+    hobby = Product.where(parent: "おもちゃ・ホビー・グッズ")
+    cosme = Product.where(parent: "コスメ・香水・美容")
+    home = Product.where(parent: "家電・スマホ・カメラ")
+    sports = Product.where(parent: "スポーツ・レジャー")
+    hands = Product.where(parent: "ハンドメイド")
+    tickets = Product.where(parent: "チケット")
+    bikes = Product.where(parent: "自動車・オートバイ")
+    others = Product.where(parent: "その他")
   end
 
   def set_search
