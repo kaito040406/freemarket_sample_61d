e1ff5e1f42dcc9593241e7492d83c506998fcae5
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2020-01-04 21:42:06 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,26 +9,35 @@ class CategoriesController < ApplicationController
   
   def show
   @product = Product.where(finished: 0).length
-  @products = Product.limit(10).order('created_at DESC')
   @products_l = Product.where(parent: 'レディース').limit(10).order('created_at DESC')
   @images_l = ProductImage.where(product_id: @products_l.ids).where.not("count > ?", 1)
   @products_m = Product.where(parent: 'メンズ').limit(10).order('created_at DESC')
   @images_m = ProductImage.where(product_id: @products_m.ids).where.not("count > ?", 1)
-  @products_b = Product.where(parent: 'ベビー・キッズ').limit(10).order('created_at DESC')
-  @images_b = ProductImage.where(product_id: @products_b.ids).where.not("count > ?", 1)
+  @products_k = Product.where(parent: 'ベビー・キッズ').limit(10).order('created_at DESC')
+  @images_k = ProductImage.where(product_id: @products_k.ids).where.not("count > ?", 1)
   @products_i = Product.where(parent: 'インテリア・住まい・小物').limit(10).order('created_at DESC')
   @images_i = ProductImage.where(product_id: @products_i.ids).where.not("count > ?", 1)
-  @products_l = Product.where(parent: '本・音楽・ゲーム').limit(10).order('created_at DESC')
-  @images_l = ProductImage.where(product_id: @products_l.ids).where.not("count > ?", 1)
-  @products_m = Product.where(parent: 'おもちゃ・ホビー・グッズ').limit(10).order('created_at DESC')
-  @images_m = ProductImage.where(product_id: @products_m.ids).where.not("count > ?", 1)
-  @products_b = Product.where(parent: 'コスメ・香水・美容').limit(10).order('created_at DESC')
+  @products_b = Product.where(parent: '本・音楽・ゲーム').limit(10).order('created_at DESC')
   @images_b = ProductImage.where(product_id: @products_b.ids).where.not("count > ?", 1)
-  @products_i = Product.where(parent: '家電・スマホ・カメラ').limit(10).order('created_at DESC')
-  @images_i = ProductImage.where(product_id: @products_i.ids).where.not("count > ?", 1)
-  # @products
+  @products_h = Product.where(parent: 'おもちゃ・ホビー・グッズ').limit(10).order('created_at DESC')
+  @images_h = ProductImage.where(product_id: @products_h.ids).where.not("count > ?", 1)
+  @products_c = Product.where(parent: 'コスメ・香水・美容').limit(10).order('created_at DESC')
+  @images_c = ProductImage.where(product_id: @products_c.ids).where.not("count > ?", 1)
+  @products_ho = Product.where(parent: '家電・スマホ・カメラ').limit(10).order('created_at DESC')
+  @images_ho = ProductImage.where(product_id: @products_ho.ids).where.not("count > ?", 1)
+  @products_s = Product.where(parent: 'スポーツ・レジャー').limit(10).order('created_at DESC')
+  @images_s = ProductImage.where(product_id: @products_s.ids).where.not("count > ?", 1)
+  @products_ha = Product.where(parent: 'ハンドメイド').limit(10).order('created_at DESC')
+  @images_ha = ProductImage.where(product_id: @products_ha.ids).where.not("count > ?", 1)
+  @products_t = Product.where(parent: 'チケット').limit(10).order('created_at DESC')
+  @images_t = ProductImage.where(product_id: @products_t.ids).where.not("count > ?", 1)
+  @products_bi = Product.where(parent: '自動車・オートバイ').limit(10).order('created_at DESC')
+  @images_bi = ProductImage.where(product_id: @products_bi.ids).where.not("count > ?", 1)
+  @products_o = Product.where(parent: 'その他').limit(10).order('created_at DESC')
+  @images_o = ProductImage.where(product_id: @products_o.ids).where.not("count > ?", 1)
+  @products
   @images = ProductImage.where(params[:id])
-  # binding.pry
+  binding.pry
   end
 
   private
